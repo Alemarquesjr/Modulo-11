07ae2f8d4ca164e943e4a02c815425c20a619d52
Alexandre Marques <marquesjurnioralexandre@gmail.com>
2023-04-25 14:08:02 -0300

    usando massa de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" /> 
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade login' , () =>{
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
   
     });
     
@@ -15,6 +16,27 @@ context('Funcionalidade login' , () =>{
         
         cy.get('#username') . type('marquesjunioralexandre@gmail.com')
         cy.get('#password') . type('rgm23281')
+        cy.get('.woocommerce-form > .button') .click()
+
+        cy.get('.page-title') .should('contain' , 'Minha conta')
+    })
+
+    it('Deve fazer login com sucesso - Usando fixture',() => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username') . type(dados.usuario)
+            cy.get('#password') . type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button') .click()
+    
+            cy.get('.page-title') .should('contain' , 'Minha conta')
+            
+        })
+
+
+    })
+
+    it.only('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username') . type(perfil.usuario)
+        cy.get('#password') . type(perfil.senha, {log: false})
         cy.get('.woocommerce-form > .button') .click()
 
         cy.get('.page-title') .should('contain' , 'Minha conta')
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,38 @@
+///<reference types="Cypress" />
+
+describe('funcionalidade Pagina de Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('produtos')
+    });
+
+    it('Deve selecionar produto  da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+
+
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3
+
+
+        cy.get('[class="product-block grid"]')
+            .contains('Atlas Fitness Tank').click()
+        cy.get('.button-variable-item-M').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Atlas Fitness Tank” foram adicionados no seu carrinho.')
+
+
+
+    });
+
+});
