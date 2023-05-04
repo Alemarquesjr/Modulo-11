a0d11d744f7bbe9c74a47946aa606e534e13bc35
Alexandre Marques <marquesjurnioralexandre@gmail.com>
2023-05-04 14:43:53 -0300

    Teste com massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,7 @@
 /// <reference types="cypress" /> 
+import enderecoPage from "../support/page-objetcs/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
+
 
 describe('Funcionalidade endereços - Faturamento e Entrega', () => {
 beforeEach(() => {
@@ -11,7 +14,26 @@ beforeEach(() => {
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
        
-        //cadastro de endereço
+        enderecoPage.editarenderecofaturamento('Alexandre', 'Marques', 'ebac', 'Brasil', 'Av São Paulo', '100', 'São Paulo', 'São Paulo', '08790310', '11978342568', 'email@dominio.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso')
+        
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPage.editarenderecofaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso')
         
     });
 });
